da694c877f28ff82e0d34407cb299bf7003dd87e
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2024-07-15 18:02:37 +0900

    1. 코드 업데이트

---
+++ SegmentationLabelViewer/Viewer/App.cs
@@ -0,0 +1,48 @@
+﻿using ConvMVVM.Core.IOC;
+using ConvMVVM.Core.Module;
+using ConvMVVM.WPF.Component;
+using ConvMVVM.WPF.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Viewer.View;
+using Viewer.ViewModel;
+
+namespace Viewer
+{
+    public class App: ConvMVVMApp
+    {
+
+        #region Protected Functions
+        protected override void ConfigureServiceCollection(IServiceCollection serviceCollection)
+        {
+            serviceCollection.RegisterCache<MainWindowViewModel>();
+        }
+
+        protected override void ConfigureModule(ModuleCollection modules)
+        {
+            
+        }
+
+
+        protected override void ConfigureServiceProvider(IServiceContainer serviceProvider)
+        {
+           
+            
+        }
+
+        protected override void ConfigureServiceLocator()
+        {
+          
+        }
+
+        protected override Window CreateWindow(IServiceContainer serviceProvider)
+        {
+            return new MainWindowView();
+        }
+        #endregion
+    }
+}
---
+++ SegmentationLabelViewer/Viewer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ConvMVVM.Core.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viewer.ViewModel
+{
+    public class MainWindowViewModel : NotifyObject
+    {
+        #region Constructor
+        public MainWindowViewModel() { 
+        
+        }
+        #endregion
+
+        #region Public Property
+
+        #endregion
+    }
+}
